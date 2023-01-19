b7d74a5988ab6b414ffb6a1dc6ba1439a25f09e8
Dani Rosa <danirosa.dev@gmail.com>
2023-01-19 20:58:25 -0300

    signin & signup

--- src/app.js
+++ src/app.js
@@ -21,6 +21,48 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.post("/sign-up", async (req, res) => {
+    const { name, email, password } = req.body
+
+    try {
+        await db.collection("usuarios").insertOne({ name, email, password })
+        res.status(201).send("Usuário cadastrado com sucesso!")
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
+app.post("/sign-in", async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const checkUser = await db.collection("usuarios").findOne({ email })
+        if (!checkUser) return res.status(400).send("Usuário ou senha incorretos.")
+        if (password !== checkUser.password) return res.status(400).send("Usuário ou senha incorretos.")
+
+        return res.status(200).send("Você logou com sucesso")
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+
+// app.post("/new-entry", async (req, res) => {
+    
+// })
+
+// app.get("/entry", async (req, res) => {
+    
+// })
+
+// app.post("/output", async (req, res) => {
+    
+// })
+
+// app.post("/output", async (req, res) => {
+    
+// })
+
+
 const PORT = 5000
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
